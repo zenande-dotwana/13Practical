74e176b4c42d9213e691998fc58c82a33763c16b
zenande-dotwana <4491773@myuwc.ac.za>
2026-02-25 15:27:12 +0200

    Enhance search algorithm performance statistics output
    Added calculations for average runtime and standard deviation for both linear and binary search algorithms. Included output formatting for performance comparison results.

--- timeMethods.java
+++ timeMethods.java
@@ -53,4 +53,31 @@ public class timeMethods {
             time = (double)(finish - start);
             binaryRunTime += time;
             binaryRunTime2 += (time * time);
+            
+            // Shuffle keys for next repetition to avoid caching effects
+            shuffleArray(testKeys);
+        }
+        
+        // Calculate statistics for linear search
+        double linearAveRuntime = linearRunTime / repetitions;
+        double linearStdDeviation = Math.sqrt((linearRunTime2 / repetitions) - 
+                                    (linearAveRuntime * linearAveRuntime));
+        
+        // Calculate statistics for binary search
+        double binaryAveRuntime = binaryRunTime / repetitions;
+        double binaryStdDeviation = Math.sqrt((binaryRunTime2 / repetitions) - 
+                                   (binaryAveRuntime * binaryAveRuntime));
+        
+        // Output results
+        System.out.println("\n\n========== SEARCH ALGORITHM PERFORMANCE COMPARISON ==========");
+        System.out.println("Testing with 30 random keys, repeated " + repetitions + " times");
+        System.out.println("------------------------------------------------");
+        System.out.println("LINEAR SEARCH (unsorted data):");
+        System.out.println("  Average time: " + fourD.format(linearAveRuntime) + " ms");
+        System.out.println("  Standard deviation: " + fourD.format(linearStdDeviation) + " ms");
+        System.out.println("\nBINARY SEARCH (sorted data):");
+        System.out.println("  Average time: " + fourD.format(binaryAveRuntime) + " ms");
+        System.out.println("  Standard deviation: " + fourD.format(binaryStdDeviation) + " ms");
+        System.out.println("------------------------------------------------");
+        System.out.println("\nFour required numbers for submission:");
         
